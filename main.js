a36c72004f5c2e09be34aeb6994ff25955b98db4
kookontheloose <141165990+kookontheloose@users.noreply.github.com>
2024-06-18 15:04:05 -0700

    Update main.js
    added try catch

--- main.js
+++ main.js
@@ -1,21 +1,31 @@
 let result = document.getElementById("inputText");
 
-let calculate = (number) => {
-  result.value += number;
-};
+const buttons = Array.from(document.querySelectorAll("button"));
 
-let Result = () => {
-  try {
-    result.value = eval(result.value);
-  } catch (err) {
-    alert("Enter the valid input");
+let calculate = (event) => {
+  switch (event.target.innerText.toLowerCase()) {
+    case "c": {
+      result.value = null;
+      break;
+    }
+    case "del": {
+      result.value = result.value.slice(0, -1);
+      break;
+    }
+    case "=": {
+      try {
+        result.value = eval(result.value);
+      } catch (err) {
+        alert("Enter the valid input");
+      }
+      break;
+    }
+    default: {
+      result.value += event.target.innerText;
+    }
   }
 };
 
-function clr() {
-  result.value = " ";
-}
-
-function del() {
-  result.value = result.value.slice(0, -1);
-}
+buttons.forEach((button) => {
+  button.addEventListener("click", calculate);
+});
